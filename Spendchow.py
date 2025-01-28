cfc1f1d5e7b81f8aac27d4c715a9ae6a5fca971c
AYu-a-74 <judycat7777@gmail.com>
2025-01-28 11:05:01 -0800

    Add main part
    Provide Spendchow file

---
+++ Spendchow.py
@@ -0,0 +1,13 @@
+import os
+Victory="\033[1;33m"
+Wasted="\033[1;35m"
+Okay="\033[0;30m"
+Chowred="\033[1;31m"
+Chowgreen="\033[1;32m"
+Chowblue="\033[1;34m"
+BluegreenChow="\033[1;36m"
+ReverseChow="\033[1;37;40m"
+def spendchow():
+    os.system('clear')
+    print(Chowgreen+"You have $10,000 and you need to control your spending so that you dont experience a force as you overspend your money. Chow Mian already assigned you a large expense on buying Physics textbok for $1,000. So now youu only have $9,000 as your current cash."+Okay)
+spendchow()
