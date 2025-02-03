c812754d9eb79e1fc1b06a799595d28241afde5e
oikazaki <janejanekw2015@gmail.com>
2025-02-03 10:34:39 -0800

    New calculation to find Total Expenses and revenue

---
+++ Spendchow.py
@@ -0,0 +1,23 @@
+import os
+Victory="\033[1;33m"
+Wasted="\033[1;35m"
+Okay="\033[0;30m"
+Chowred="\033[1;31m"
+Chowgreen="\033[1;32m"
+Chowblue="\033[1;34m"
+BluegreenChow="\033[1;36m"
+ReverseChow="\033[1;37;40m"
+def spendchow():
+    os.system('clear')
+    print(Chowgreen+"You have $10,000 and you need to control your spending so that you dont experience a force as you overspend your money. Chow Mian already assigned you a large expense on buying Physics textbok for $1,000. So now youu only have $9,000 as your current cash."+Okay)
+spendchow()
+
+TotalExpenses = WhatYouNeed [0] + WhatYouNeed [1] + WhatYouNeed [2] + WhatYouNeed [3] + WhatYouNeed [4] + WhatYouNeed [5] + WhatYouNeed [6] + WhatYouNeed [7] + WhatYouNeed [8] + WhatYouNeed [9] + WhatYouNeed [10]
+Revenue = NetIncome - TotalExpenses
+
+
+
+
+
+
+
